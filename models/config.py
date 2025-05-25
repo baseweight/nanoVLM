7fa2fa1aa2b3a481c96e5231a13d14ddc0f6728f
Joe Bowser <bowserj@gmail.com>
2025-05-25 15:44:15 -0700

    Minor update to config

--- models/config.py
+++ models/config.py
@@ -38,7 +38,7 @@ class VLMConfig:
 
     vlm_load_backbone_weights: bool = True
     vlm_checkpoint_path: str = 'checkpoints/nanoVLM-222M'
-    hf_repo_name: str = 'nanoVLM'
+    hf_repo_name: str = 'infil00p/nanoVLM'
 
 
 @dataclass
@@ -59,5 +59,5 @@ class TrainConfig:
     train_dataset_path: str = 'HuggingFaceM4/the_cauldron'
     train_dataset_name: tuple[str, ...] = ("ai2d", "aokvqa", "chart2text", "chartqa", "clevr", "cocoqa", "datikz", "diagram_image_to_text", "docvqa", "dvqa", "figureqa", "finqa", "geomverse", "hateful_memes", "hitab", "iam", "iconqa", "infographic_vqa", "intergps", "localized_narratives", "mapqa", "multihiertt", "ocrvqa", "plotqa", "raven", "rendered_text", "robut_sqa", "robut_wikisql", "robut_wtq", "scienceqa", "screen2words", "st_vqa", "tabmwp", "tallyqa", "tat_qa", "textcaps", "textvqa", "tqa", "vistext", "visual7w", "visualmrc", "vqarad", "vqav2", "vsr", "websight")
     test_dataset_path: str = "Lin-Chen/MMStar"
-    wandb_entity: str = "HuggingFace" # Indicate the entity to log to in wandb
+    wandb_entity: str = "baseweight-ai" # Indicate the entity to log to in wandb
     log_wandb: bool = True
